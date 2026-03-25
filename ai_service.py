b43b54fbc7f7b9e0fd2f870455e3501d68dcd5b2
HG <azure5finger@gmail.com>
2026-03-25 14:19:19 +0900

    #8 router prefix ai_service로 변경

--- ai_service.py
+++ ai_service.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-router = APIRouter(prefix="/ai", tags=["ai"])
+router = APIRouter(prefix="/ai_service", tags=["ai_service"])
 
 
 # -----------------------------
