4fe93223ee6faa2b9ad40eab3d39cbccea4ddbba
HG <azure5finger@gmail.com>
2026-03-26 14:09:35 +0900

    #10 gitignore tracking 수정

--- conftest.py
+++
@@ -1,9 +0,0 @@
-# conftest.py
-import pytest
-
-@pytest.fixture(autouse=True)
-def mock_generate_ai_message(monkeypatch):
-    def fake_ai_message(*args, **kwargs):
-        return "테스트용 AI 메시지입니다."
-
-    monkeypatch.setattr("ai_service.generate_ai_message", fake_ai_message)
